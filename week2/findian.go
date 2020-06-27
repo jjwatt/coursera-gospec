0022995c0154649243dc5c19dbce95174a4bd70c
Jesse Wattenbarger <jwatt@broken.watch>
2020-06-26 20:09:48 -0400

    Eliminate some repetition in findian.
    Still don't love it. Could have just used if.

--- week2/findian.go
+++ week2/findian.go
@@ -14,18 +14,20 @@ func main() {
 	input := scanner.Text()
 	// fmt.Println("captured: ", input)
 	low_input := strings.ToLower(input)
+	not_found := false
 	switch {
 	case strings.Index(low_input, "i") != 0:
-		fmt.Println("Not Found!")
-		os.Exit(1)
+		not_found = true
 	case strings.LastIndex(low_input, "n") != len(low_input) - 1:
-		fmt.Println("Not Found!")
-		os.Exit(1)
+		not_found = true
 	case strings.Contains(low_input, "a") == false:
-		fmt.Println("Not Found!")
-		os.Exit(1)
+		not_found = true
 	default:
 		fmt.Println("Found!")
 		os.Exit(0)
+	}
+	if not_found == true {
+		fmt.Println("Not Found!")
+		os.Exit(1)
 	}
 }
