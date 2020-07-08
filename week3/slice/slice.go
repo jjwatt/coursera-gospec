1b6cadd3cd090acd98b352a189fb4a3ae7fc1fff
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-07-08 16:17:23 -0400

    Fix bug checking for "X"

--- week3/slice/slice.go
+++ week3/slice/slice.go
@@ -16,19 +16,19 @@ func main() {
 		scanner.Scan()
 		userin := scanner.Text()
 		lowin := strings.ToLower(userin)
-		if lowin == "X" {
+		if lowin == "x" {
 			break
 		}
 		num, err := strconv.Atoi(lowin)
 		if err != nil {
-			fmt.Printf("Error: %v", err)
+			fmt.Printf("Error: %v\n", err)
 			fmt.Println("Please enter a number or 'X' to quit")
 			continue
 		}
 		li = append(li, num)
 		sort.Ints(li)
-		fmt.Printf("%v", li)
+		fmt.Printf("%v\n", li)
 	}
-	fmt.Printf("%v", li)
+	fmt.Printf("%v\n", li)
 	os.Exit(0)
 }
